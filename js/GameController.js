eb564f4e9434b30d3e27d1eb83755f6091ba4f02
Kevin Portugal <portugalkevin@gmail.com>
2015-05-18 08:07:37 -0700

    game working

--- js/GameController.js
+++ js/GameController.js
@@ -4,38 +4,77 @@ angular
 
 	GameController.$inject = ['$firebaseObject', '$scope'];
 
-	function GameController($firebaseObject, $scope){
+function GameController($firebaseObject, $scope){
 
 	var ref = new Firebase('https://dazzling-heat-8209.firebaseio.com');
-	
+
 	$firebaseObject(ref).$bindTo($scope, "game");
+	
 
-	var playerOne = 1
-	var playerTwo = -1
+    $scope.playerMove = function(location) {
+		//switches player 1 turn on and off
+    	//occupied square cannot be played
+    	//
+		if ($scope.game.playerOneTurn && $scope.game.gameBoard[location].cellContent === " ") {
+			$scope.game.gameBoard[location].cellValue = $scope.game.playerOne;
+			$scope.game.gameBoard[location].cellContent = "X"
+			$scope.game.playerOneTurn = false;
+		} else if ($scope.game.gameBoard[location].cellContent === " "){
+			$scope.game.gameBoard[location].cellValue = $scope.game.playerTwo;
+			$scope.game.gameBoard[location].cellContent = "O"
+			$scope.game.playerOneTurn = true;
+		}
+		$scope.game.winner = getWinner();
+	 }
 
-	$scope.gameBoard = [
-		
-		{cell: null}, 
-		{cell: null}, 
-		{cell: null}, 
-		{cell: null}, 
-		{cell: null}, 
-		{cell: null}, 
-		{cell: null}, 
-		{cell: null}, 
-		{cell: null}
-	];
-
-
-function winningTrio(box1, box2, box3) {
-    if (Math.abs(box1 + box2 + box3) === 3) {
-        if (box1 === 1) {
-            return "Player One Wins!"
-        } else {
-            return "Player Two Wins!"
+	$scope.clearBoard = function() {
+	 	// when the button is clicked, the cell values reset to 0, and playerOneTurn resets to true.
+	 	for (var i = 0; i < 9; i++) {
+            $scope.game.gameBoard[i].cellContent = " ";
+            $scope.game.gameBoard[i].cellValue = 0;
         }
-    }
-}
-
+        $scope.game.playerOneTurn = true;
+        $scope.game.winner = 0;
 	}
+
+	$scope.resetScore = function() {
+		$scope.game.playerOneWinTotal = 0;
+		$scope.game.playerTwoWinTotal = 0;
+	}
+
+	function getWinner() {
+		var board = $scope.game.gameBoard;
+		var winner = 0;
+		winner = winningTrio(board[0].cellValue, board[1].cellValue, board[2].cellValue);
+		if (winner) return winner;
+		winner = winningTrio(board[3].cellValue, board[4].cellValue, board[5].cellValue);
+		if (winner) return winner;
+		winner = winningTrio(board[6].cellValue, board[7].cellValue, board[8].cellValue);
+		if (winner) return winner;
+		winner = winningTrio(board[0].cellValue, board[3].cellValue, board[6].cellValue);
+		if (winner) return winner;
+		winner = winningTrio(board[1].cellValue, board[4].cellValue, board[7].cellValue);
+		if (winner) return winner;
+		winner = winningTrio(board[2].cellValue, board[5].cellValue, board[8].cellValue);
+		if (winner) return winner;
+		winner = winningTrio(board[6].cellValue, board[4].cellValue, board[2].cellValue);
+		if (winner) return winner;
+		winner = winningTrio(board[0].cellValue, board[4].cellValue, board[8].cellValue);
+		if (winner) return winner;
+		return 0;
+	}
+
+	function winningTrio(box1, box2, box3) {
+    	if (Math.abs(box1 + box2 + box3) === 3) {
+        	if (box1 === 1) {
+        		$scope.game.playerOneWinTotal++;
+            	return 1;
+        	} else if (box1 === -1) {
+            	$scope.game.playerTwoWinTotal++;
+            	return -1;
+        	}
+    	}	
+	}
+
+}
 

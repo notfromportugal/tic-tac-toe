ecbe466bce2fc7b64e837f9f84dcfe01c442f755
Kevin Portugal <portugalkevin@gmail.com>
2015-05-18 09:56:24 -0700

    app at presentation

--- js/GameController.js
+++ js/GameController.js
@@ -12,9 +12,9 @@ function GameController($firebaseObject, $scope){
 	
 
     $scope.playerMove = function(location) {
-		//switches player 1 turn on and off
-    	//occupied square cannot be played
-    	//
+		//switches player 1 turn on and off to rotate turns
+    	//square must have no content to be played
+    	//assigns value of 1 or -1 for playerOne and Two
 		if ($scope.game.playerOneTurn && $scope.game.gameBoard[location].cellContent === " ") {
 			$scope.game.gameBoard[location].cellValue = $scope.game.playerOne;
 			$scope.game.gameBoard[location].cellContent = "X"
@@ -28,7 +28,7 @@ function GameController($firebaseObject, $scope){
 	 }
 
 	$scope.clearBoard = function() {
-	 	// when the button is clicked, the cell values reset to 0, and playerOneTurn resets to true.
+	 	// when the button is clicked, the cell values reset to 0, content to empty, and playerOneTurn resets to true.
 	 	for (var i = 0; i < 9; i++) {
             $scope.game.gameBoard[i].cellContent = " ";
             $scope.game.gameBoard[i].cellValue = 0;
@@ -38,11 +38,13 @@ function GameController($firebaseObject, $scope){
 	}
 
 	$scope.resetScore = function() {
+		//resets win totals to 0 when clicked
 		$scope.game.playerOneWinTotal = 0;
 		$scope.game.playerTwoWinTotal = 0;
 	}
 
 	function getWinner() {
+		//checks each winning combo against winningTrio
 		var board = $scope.game.gameBoard;
 		var winner = 0;
 		winner = winningTrio(board[0].cellValue, board[1].cellValue, board[2].cellValue);
@@ -65,6 +67,7 @@ function GameController($firebaseObject, $scope){
 	}
 
 	function winningTrio(box1, box2, box3) {
+		//checks if absolute value of any getWinner combos is 3, and returns a winner value if so
     	if (Math.abs(box1 + box2 + box3) === 3) {
         	if (box1 === 1) {
         		$scope.game.playerOneWinTotal++;
@@ -75,6 +78,5 @@ function GameController($firebaseObject, $scope){
         	}
     	}	
 	}
-
 }
 
